9ea837770cdb9520d5239754933eb99ed64f7327
Mora Joseph <morajoseph72@gmail.com>
2025-04-16 16:35:10 +0200

    purchase

--- Models/Purchase.cs
+++ Models/Purchase.cs
@@ -1,6 +1,21 @@
-﻿namespace BookStore.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStore.Models
 {
     public class Purchase
     {
+        public int Id { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        [ForeignKey("Book")]
+        public int BookId { get; set; }
+        public DateTime PurchaseDate { get; set; }
+
+        //-----------
+
+        public ApplicationUser User { get; set; }
+        public Book Book { get; set; }
+
+
     }
 }
